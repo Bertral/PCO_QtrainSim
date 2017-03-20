335543e96e46a46d25456b11706ea2dda16ac2bc
Bertral <antoine.friant@gmail.com>
2017-03-20 16:01:18 +0100

    trajectoire

--- src/student_cpp/cppmain.cpp
+++ src/student_cpp/cppmain.cpp
@@ -4,12 +4,14 @@
 #include <QList>
 
 //Creation d'une locomotive
-static Locomotive locomotive;
+static Locomotive loco1;
+static Locomotive loco2;
 
 //Arret d'urgence
 void emergency_stop()
 {
-    locomotive.arreter();
+    loco1.arreter();
+    loco2.arreter();
     afficher_message("\nSTOP!");
 }
 
@@ -28,43 +30,46 @@ int cmain()
     //Initialisation des aiguillages
     diriger_aiguillage(1, DEVIE, 0);
     diriger_aiguillage(2, DEVIE, 0);
-    diriger_aiguillage(3, DEVIE, 0);
-    diriger_aiguillage(4, DEVIE, 0);
-    diriger_aiguillage(5, TOUT_DROIT, 0);
-    diriger_aiguillage(6, TOUT_DROIT, 0);
-    diriger_aiguillage(7, DEVIE, 0);
-    diriger_aiguillage(8, DEVIE, 0);
+    diriger_aiguillage(5, DEVIE, 0);
+    diriger_aiguillage(6, DEVIE, 0);
     diriger_aiguillage(9, DEVIE, 0);
-    diriger_aiguillage(10, TOUT_DROIT, 0);
-    diriger_aiguillage(11, TOUT_DROIT, 0);
-    diriger_aiguillage(12, DEVIE, 0);
+    diriger_aiguillage(10, DEVIE, 0);
     diriger_aiguillage(13, DEVIE, 0);
     diriger_aiguillage(14, DEVIE, 0);
-    diriger_aiguillage(15, TOUT_DROIT, 0);
-    diriger_aiguillage(16, TOUT_DROIT, 0);
     diriger_aiguillage(17, TOUT_DROIT, 0);
-//    diriger_aiguillage(18, TOUT_DROIT,  0);
-//    diriger_aiguillage(19, TOUT_DROIT,  0);
 
     //Initialisation de la locomotive
-    locomotive.fixerNumero(1);
-    locomotive.fixerVitesse(12);
-    locomotive.fixerPosition(15, 10);
-    locomotive.allumerPhares();
-    locomotive.demarrer();
-    locomotive.afficherMessage("Ready!");
+    loco1.fixerNumero(1);
+    loco1.fixerVitesse(12);
+    loco1.fixerPosition(10, 15);
+    loco1.allumerPhares();
+    loco1.demarrer();
+    loco1.afficherMessage("Ready!");
+
+    loco2.fixerNumero(2);
+    loco2.fixerVitesse(12);
+    loco2.fixerPosition(1, 19);
+    loco2.allumerPhares();
+    loco2.demarrer();
+    loco2.afficherMessage("Ready!");
 
     //Attente du passage sur les contacts
     for (int i = 1; i < parcours.size(); i++) {
         attendre_contact(parcours.at(i));
         afficher_message(qPrintable(QString("The engine no. %1 has reached contact no. %2.")
-                                    .arg(locomotive.numero()).arg(parcours.at(i))));
-        locomotive.afficherMessage(QString("I've reached contact no. %1.").arg(parcours.at(i)));
+                                    .arg(loco1.numero()).arg(parcours.at(i))));
+        afficher_message(qPrintable(QString("The engine no. %1 has reached contact no. %2.")
+                                    .arg(loco2.numero()).arg(parcours.at(i))));
+        loco1.afficherMessage(QString("I've reached contact no. %1.").arg(parcours.at(i)));
+        loco2.afficherMessage(QString("I've reached contact no. %1.").arg(parcours.at(i)));
     }
 
     //Arreter la locomotive
-    locomotive.arreter();
-    locomotive.afficherMessage("Yeah, piece of cake!");
+    loco1.arreter();
+    loco1.afficherMessage("Yeah, piece of cake!");
+
+    loco2.arreter();
+    loco2.afficherMessage("Yeah, piece of cake!");
 
     //Fin de la simulation
     mettre_maquette_hors_service();
